76b05eeb16e51b635c78d4ca4eaa25e5ef7a87d0
JoaquinDucos <87881536+JoaquinDucos@users.noreply.github.com>
2022-11-21 12:57:53 -0300

    Update index.js

--- request_testing/index.js
+++ request_testing/index.js
@@ -54,7 +54,7 @@ app.listen(
 
 // Agrega un endpoint, "ruta" al final (por eso endpoint) del localhost
 app.get('/tshirt', (req, res) => {  //Tenemos acceso a dos objetos
-                                    // REQuest: incoming data y RESponse outgoin data(The one we want to send back to the client
+                                    // REQuest: incoming data y RESponse outgoin data(The one we want to send back to the client)
     res.status(200).send({
         tshirt:  'River Plate',
         size: 'XS'
@@ -62,11 +62,11 @@ app.get('/tshirt', (req, res) => {  //Tenemos acceso a dos objetos
 });
 
 // con un POST request significa que el usuario está intentando crear nueva información en el servidor( este caso, una nueva remera)
-// :id es un dynamic URL parameter que respresenta el ID de la remera.
+// :id es una dynamic URL parameter que respresenta el ID de la remera.
 app.post('/tshirt/:id', (req, res) => {
 
     const { id } = req.params;
-    const { logo } = req.body;  //El problema es que express no parsea json por default. Necesitamos un middleware
+    const { logo } = req.body;  //El problema es que express no parsea json por default. Necesitamos un middleware para realizar el parsing.
 
     if (!logo) {
         res.status(418).send({message: 'We need a logo!'})
